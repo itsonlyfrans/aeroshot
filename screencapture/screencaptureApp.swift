19fd72b42833078ff8c564a38233e36807965c41
Frans <46535667+itsonlyfrans@users.noreply.github.com>
2026-07-04 12:34:37 -0400

    Initial Commit

---
+++ screencapture/screencaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  screencaptureApp.swift
+//  screencapture
+//
+//  Created by Frans on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct screencaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ screencaptureTests/screencaptureTests.swift
@@ -0,0 +1,19 @@
+//
+//  screencaptureTests.swift
+//  screencaptureTests
+//
+//  Created by Frans on 7/4/26.
+//
+
+import Testing
+@testable import screencapture
+
+struct screencaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
